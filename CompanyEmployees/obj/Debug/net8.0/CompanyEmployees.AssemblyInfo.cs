047da0eedb6f0e3d3b5b3ab68cb9e408a874669d
Fábio Dias <fabiodias1296@gmail.com>
2026-01-04 19:53:33 +0000

    Added Patch Request attribute validation for Employees

--- CompanyEmployees/obj/Debug/net8.0/CompanyEmployees.AssemblyInfo.cs
+++ CompanyEmployees/obj/Debug/net8.0/CompanyEmployees.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95b19026dce30bdbf7e8ae3cba2633c0cb88773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b60069530f2e54419bee2c06b9109000b05227f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
